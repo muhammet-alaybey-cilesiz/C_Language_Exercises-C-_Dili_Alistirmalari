0b540777544cd686b1a40774f748f1ae66244276
Muhammet A. Çilesiz <240522006@firat.edu.tr>
2025-04-24 16:43:07 +0300

    Add files via upload

---
+++ char_array1.8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Program that prints the name and author of the book
+// Kitap adını ve yazarını ekrana yazdıran program
+
+int main(){
+    char book_name[40]="Mechatronics Engineering";
+	char author_name[40]="Engineer Mehmet";
+	
+	printf("Book Name: %s\n",book_name);
+	printf("Author Name: %s\n",author_name);
+
+    return 0;
+}
