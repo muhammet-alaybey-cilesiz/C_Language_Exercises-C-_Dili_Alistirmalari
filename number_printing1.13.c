a2ba0703b5f578ea9d282edacb238e022e3456e5
Muhammet A. Çilesiz <240522006@firat.edu.tr>
2025-05-10 19:12:08 +0300

    Add files via upload

---
+++ number_printing1.13.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Program that prints numbers in desired range and step size
+//İstenilen aralıkta ve adım boyutunda sayıları yazdıran program
+
+int main(){
+	int start,end,step_size;
+	while(1){
+		printf("Start value: ");
+		scanf("%d",&start);
+		printf("End value: ");
+		scanf("%d",&end);
+		printf("Step size: ");
+	    scanf("%d",&step_size);
+		if(step_size == 0){
+			printf("Step size cannot be zero. Please enter a valid value.\n"); //Adım boyutu sıfır olamaz. Lütfen geçerli bir değer girin.
+		}
+		else if (step_size < 0 && end > start){
+			printf("Must be in countdown (start > finish)! Please enter a valid value.\n"); //Geri sayımda (başlangıç ​​> bitiş) olmalı.
+		}
+		else if (step_size > 0 && start > end){
+			printf("must be (end > start)! Please enter a valid value.\n");                 // (bitiş > başlangıç) olmalı. 
+		}
+		else {
+			break;
+		}
+    }
+
+	if(step_size > 0){
+		for(int i=start; i<=end; i+=step_size){
+		printf("%d\n",i);
+		}
+	}
+	else{
+		for(int i=start; i>=end; i+=step_size){
+		printf("%d\n",i);
+	    }
+	}
+
+    return 0;
+}
---
+++ switch1.14.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Switch - Case Structure
+//Switch - Case Yapısı 
+
+int main(){
+	float a;                  // a : avarage / ortalama (0-100)
+	printf("a: ");
+	scanf("%f",&a);
+	
+	switch((int)a/10){        // The value of "switch" must be an integer, so (int) is used .    
+		case 10:              //"switch" değeri tam sayı olmalıdır, bu nedenle (int) kullanıldı .
+		case 9: 
+		 printf("AA");        // 90 <= avarage <= 100 : AA
+		break; 
+	    case 8:
+		case 7: 
+		 printf("BB");        // 65 <= avarage < 90 : BB
+
+		break;
+        case 6:
+        	if(a>=65){
+			printf("BB");	   
+		    } 
+		    else{
+		    printf("CC");     // 60 <= avarage < 65  : CC 
+ 		    }
+		break;
+		case 5:
+		 printf("DD");        // 50 <= avarage < 60 : DD    
+		break;
+		case 4:
+		case 3:
+		case 2:
+		case 1:
+		case 0: 	
+		 printf("FF");        // 0 <= avarage < 50 : FF  
+		break;     	 
+	}
+	return 0;
+}
